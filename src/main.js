652bb93074063c93015eb2b5d2593d61844e32bf
LongShaoJun <7694learner@163.com>
2020-03-20 18:25:00 +0800

    修改import

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui';
 import "element-ui/lib/theme-chalk/index.css"
-import("./css/style.css");
+require("./css/style.css");
 import App from './App';
 import router from './router';
 
